f2074f677a58f73d0e28ee6fe7a9ad587a09139d
spizarrop <santiagopizarropizarro.guadalupe@alumnado.fundacionloyola.net>
2025-10-15 20:45:06 +0200

    final

--- futbolistas.js
+++ futbolistas.js
@@ -23,7 +23,7 @@ const futbolistas = [
         partidosJugados: [
             { rival: "Al Hilal", goles: 1, asistencias: 0, tarjetaAmarilla: true, tarjetaRoja: false },
             { rival: "Al Shabab", goles: 2, asistencias: 1, tarjetaAmarilla: false, tarjetaRoja: false },
-            { rival: "Al Ahli", goles: 0, asistencias: 1, tarjetaAmarilla: false, tarjetaRoja: true }
+            { rival: "Al Ahli", goles: 1, asistencias: 1, tarjetaAmarilla: false, tarjetaRoja: true }
         ]
     },
     {
@@ -42,25 +42,78 @@ const futbolistas = [
  * ACT 1 - EXTRA: Calcular el total de goles y asistencias de cada jugador: Usa map() y reduce() para calcular el total de goles y asistencias de cada jugador en sus partidos.
  */
 function totalGolesAsistenciasJugador() {
-    
-}
+    const jugadores = [];
 
-function totalGolesAsistenciasPartidos() {
-    
+    futbolistas.forEach(jugador => {
+        let goles = jugador.partidosJugados.reduce((acc, total) => acc + total.goles, 0);
+        let asistencias = jugador.partidosJugados.reduce((acc, total) => acc + total.asistencias, 0);
+        jugadores.push({ nombre: jugador.nombre, totalGoles: goles, totalAsistencias: asistencias });
+    });
+
+    console.log(jugadores);
 }
+//totalGolesAsistenciasJugador();
 
 /**
  * ACT 2 - EXTRA: Filtrar jugadores con tarjetas rojas en algún partido: Encuentra a los jugadores que hayan recibido al menos una tarjeta roja en algún partido.
  */
+function tarjetasRojas() {
+    const jugadorConRoja = [];
+
+    futbolistas.forEach(jugador => {
+        if (jugador.partidosJugados.filter(partido => partido.tarjetaRoja == true).length > 0) {
+            jugadorConRoja.push(jugador.nombre);
+        };
+    });
+
+    console.log(jugadorConRoja);
+}
+//tarjetasRojas();
 
 /**
  * ACT 3 - EXTRA: Listar los rivales en los que un jugador específico anotó: Pide el nombre de un jugador y, si existe en el array, devuelve una lista de los equipos rivales contra los que anotó.
  */
+function anotoJugador(nombreJugador) {
+    let listaRivales = [];
+
+    futbolistas.forEach(jugador => {
+        if(jugador.nombre == nombreJugador){
+            listaRivales = jugador.partidosJugados.filter(partido => partido.goles > 0).map(partido => partido.rival);
+        }
+    });
+
+    console.log(listaRivales);
+}
+//anotoJugador("Cristiano Ronaldo");
 
 /**
  * ACT 4 - EXTRA: Calcular el promedio de tarjetas amarillas por jugador en sus partidos: Calcula el promedio de tarjetas amarillas recibidas en los partidos para cada jugador.
  */
+function promedioTarjetas() {
+    const tarjetasAmarillas = [];
+
+    futbolistas.forEach(jugador => {
+        let amarillasJugador = jugador.partidosJugados.filter(partido => partido.tarjetaAmarilla == true).reduce((acc, partido) => acc + (partido.tarjetaAmarilla ? 1 : 0), 0);
+        let promedio = amarillasJugador / jugador.partidosJugados.length;
+        tarjetasAmarillas.push({nommbre: jugador.nombre, promedioAmarillas: promedio.toFixed(2)});
+    });
+
+    console.log(tarjetasAmarillas);
+}
+//promedioTarjetas();
 
 /**
  * ACT 5 - EXTRA: Contar los jugadores con al menos un gol en todos sus partidos: Filtra los jugadores que hayan anotado al menos un gol en cada uno de sus partidos.
  */
+function contarJugadores() {
+    const goleadores = [];
+
+    futbolistas.forEach(jugador => {
+        if (jugador.partidosJugados.filter(partido => partido.goles > 0).length == jugador.partidosJugados.length){
+            goleadores.push(jugador.nombre);
+        }
+    });
+    
+    console.log(goleadores);
+}
+//contarJugadores();
